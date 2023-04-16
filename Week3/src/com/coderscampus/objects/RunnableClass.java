e6de5cf25d35be0b6ad82581549e06ba7e9c2182
NewCodeMike <newcodemike@gmail.com>
2023-04-16 19:14:08 -0400

    End of lesson 3-1

--- Week3/src/com/coderscampus/objects/RunnableClass.java
+++ Week3/src/com/coderscampus/objects/RunnableClass.java
@@ -3,7 +3,29 @@ package com.coderscampus.objects;
 public class RunnableClass {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
+		// Class -> Blueprint form an Object
+		// Object -> Noun, it's an instantiation of a Class
+		
+		// Data type, Variable name, assignment operation
+		// int someNumber = 39;
+		
+		Human trevorPage = new Human();
+		
+		trevorPage.eyeColor = "Brown";
+		trevorPage.gender = "Male";
+		trevorPage.height = "5'10\"";
+		trevorPage.weight = "160";
+		
+		System.out.println(trevorPage);
+		
+		Human elonMusk = new Human();
+		
+		elonMusk.eyeColor = "blue";
+		elonMusk.gender = "M=male";
+		elonMusk.height = "6'1\"";
+		elonMusk.weight = "190";
+		
+		System.out.println(elonMusk);
 }
 
 }
